9fd11ab74104ea0862cb5db9ebcb504607450ea0
brunabxs <brunabxsantos@gmail.com>
2017-08-02 18:11:26 -0300

    teste

--- release.sh
+++ release.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-  git checkout ${TRAVIS_BRANCH}
-}
-
-upload_files() {
-  git remote add origin-travis https://${GH_TOKEN}@github.com/brunabxs/travis-github-push.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-travis ${TRAVIS_BRANCH}
-}
-
-setup_git
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git checkout ${TRAVIS_BRANCH}
 gulp release
-upload_files
+git remote add origin-travis https://${GH_TOKEN}@github.com/brunabxs/travis-github-push.git > /dev/null 2>&1
+git push --quiet --set-upstream origin-travis ${TRAVIS_BRANCH}
+git push --quiet --set-upstream origin-travis --tags
